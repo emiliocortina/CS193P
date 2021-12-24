c83f83123ebe0807c783e28cd412f40165488878
Emilio Cortina Labra <emicorti@amazon.com>
2021-12-24 16:51:55 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Cortina Labra, Emilio on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
